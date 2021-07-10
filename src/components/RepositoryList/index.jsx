c973f21af2a27a56a642bcd305c78cac1d7ae58f
Guigalaverna <guigalaverna@gmail.com>
2021-07-10 19:28:59 -0300

    Create repository list

---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default RepositoryList;
